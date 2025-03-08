4c1bcde91bae97cfdcc53d5bdf50e4af4c5af9a3
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-03-08 14:16:15 -0600

    undoing updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,18 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked', // or strict-type-checked
-    'plugin:@typescript-eslint/stylistic-type-checked', // optional for stylistic rules
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  plugins: ['react-refresh'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'react/react-in-jsx-scope': 'off'  
   },
 };
