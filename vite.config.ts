63a6aa7cb18dbdb0074bd5defd16a123db94c0ca
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-03-14 18:15:38 -0500

    resolving vite.config error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: '.',
   plugins: [react()],
+  preview: {
+    allowedHosts: ['candidate-search-ihmu.onrender.com'],
+    host: true // This ensures the preview server binds to all network interfaces
+  }
 });
